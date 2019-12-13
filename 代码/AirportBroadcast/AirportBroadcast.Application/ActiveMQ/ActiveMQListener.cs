5304e05283b8fb14c4442e5126abc6cff91bff07
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-13 12:52:13 +0800

    航显数据对接接口测试完成

--- 代码/AirportBroadcast/AirportBroadcast.Application/ActiveMQ/ActiveMQListener.cs
+++ 代码/AirportBroadcast/AirportBroadcast.Application/ActiveMQ/ActiveMQListener.cs
@@ -222,9 +222,8 @@ namespace AirportBroadcast.ActiveMQ
         }
 
 
-        
         public void DoMsg(string json)
-        {
+        { 
             //获取到ActiveMQ数据了
             //todo: MQ内获取到的航班信息内容解析
             if (!string.IsNullOrEmpty(json))
