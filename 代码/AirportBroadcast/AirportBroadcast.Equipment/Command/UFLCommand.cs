144914c8b863b95784bbdb994ac55b5fb670709a
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-16 16:39:20 +0800

    更新航班指令解析完成

--- 代码/AirportBroadcast/AirportBroadcast.Equipment/Command/UFLCommand.cs
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment/Command/UFLCommand.cs
@@ -17,11 +17,54 @@ namespace AirportBroadcast.Equipment
         public virtual string CommandString { get; set; }
 
         /// <summary>
+        /// 航班信息
+        /// </summary>
+        public virtual FlightInfo FlightInfo { get; set; }
+
+        /// <summary>
         /// 解析指令内容
         /// </summary>
         public virtual void Analysis()
         {
+            var data = CommandString.Substring(1 + 5 + 3);//去除头部
 
+            //解析定位条件 航班的URNO 10位 + FLNO 航班号 9位 + YYYYMMDDhhmmss  14位 + A 表示进港，D表示出港 1位
+            var condition = data.Substring(0, 10 + 9 + 14 + 1);
+
+            FlightInfo = new FlightInfo();
+            FlightInfo.URNO = condition.Substring(0, 10).Trim();//航班的URNO 10位
+            FlightInfo.FLNO = condition.Substring(10, 9).Trim();//FLNO 航班号 9位 
+            FlightInfo.AORD = condition.Substring(10 + 9 + 14, 1);//A 表示进港，D表示出港 1位
+
+            var time = condition.Substring(10 + 9, 14);// YYYYMMDDhhmmss  14位
+            if (FlightInfo.AORD == "A")
+            {
+                FlightInfo.STOA = time;
+            }
+            else
+            {
+                FlightInfo.STOD = time;
+            }
+            data = data.Substring(10 + 9 + 14 + 1);//去掉定位条件
+
+            var fieldValues = data.Split(new char[] { '#' }, StringSplitOptions.RemoveEmptyEntries);
+
+            FlightInfo = new FlightInfo();
+            var type = FlightInfo.GetType();
+
+            var i = 0;
+            foreach (var fieldValue in fieldValues)
+            {
+                var temp = fieldValue.Split('=');
+                var field = temp[0];//属性名称
+                var value = temp[1];//属性对应的值
+                var property = type.GetProperty(field);
+                if (property != null)
+                {
+                    i++;
+                    property.SetValue(FlightInfo, value);
+                }
+            }
         }
     }
 }
