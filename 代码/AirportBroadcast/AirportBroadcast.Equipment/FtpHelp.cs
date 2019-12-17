be9e080c4737a44f5269f9c14a2a40fe54f8869b
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-17 14:24:30 +0800

    增加FTP和INI帮助类

---
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment/FtpHelp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBroadcast.Equipment
+{
+    /// <summary>
+    /// FTP帮助类
+    /// </summary>
+    public class FtpHelp
+    {
+    }
+}
