bcfe248697b6f4b46a75b22046ea1c53b41cfa48
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-17 09:49:30 +0800

    航班服务解析

---
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment/Data/FlightDescribe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBroadcast.Equipment
+{
+    /// <summary>
+    /// SV航班服务基础数据
+    /// </summary>
+    public class FlightDescribe
+    {
+        #region SV航班服务基础数据字段
+
+        /// <summary>
+        /// 服务代码
+        /// </summary>
+        public string SRNU { get; set; }
+
+        /// <summary>
+        /// 服务中文描述
+        /// </summary>
+        public string SRDC { get; set; }
+
+        /// <summary>
+        /// 服务英文描述
+        /// </summary>
+        public string SRDE { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment/Data/UrgeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBroadcast.Equipment
+{
+    /// <summary>
+    /// 催促信息
+    /// </summary>
+    public class UrgeInfo
+    {
+    }
+}
