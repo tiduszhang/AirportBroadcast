8afe6fa4c57149cf1d48f47f651bab6317f88ac0
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-18 13:46:47 +0800

    Window服务类

---
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment.Service/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBroadcast.Equipment.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            new Service1().Debug();
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
