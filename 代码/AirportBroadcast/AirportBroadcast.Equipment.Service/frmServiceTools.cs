a144e8ff2f542ba4795e1451906a5c91c7a58f3e
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-18 15:57:46 +0800

    提示信息

--- 代码/AirportBroadcast/AirportBroadcast.Equipment.Service/frmServiceTools.cs
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment.Service/frmServiceTools.cs
@@ -18,9 +18,15 @@ namespace AirportBroadcast.Equipment.Service
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// 服务名称
+        /// </summary>
         private string serverName = "AirportBroadcast.Equipment.Service";
-
+        /// <summary>
+        /// 注册服务
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnInstall_Click(object sender, EventArgs e)
         {
             Process process = new Process();
@@ -28,8 +34,13 @@ namespace AirportBroadcast.Equipment.Service
             process.StartInfo.FileName = "Install.bat";
             process.StartInfo.CreateNoWindow = true;
             process.Start();
+            MessageBox.Show("服务已注册。", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
+        /// <summary>
+        /// 卸载服务
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnUnInstall_Click(object sender, EventArgs e)
         {
             Process process = new Process();
@@ -37,14 +48,24 @@ namespace AirportBroadcast.Equipment.Service
             process.StartInfo.FileName = "Uninstall.bat";
             process.StartInfo.CreateNoWindow = true;
             process.Start();
+            MessageBox.Show("服务已注销。", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
+        /// <summary>
+        /// 启动服务
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnStart_Click(object sender, EventArgs e)
         {
             ServiceController serviceController = new ServiceController(serverName);
             serviceController.Start();
+            MessageBox.Show("服务已启动。", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
+        /// <summary>
+        /// 停止服务
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnStop_Click(object sender, EventArgs e)
         {
             ServiceController serviceController = new ServiceController(serverName);
@@ -52,8 +73,13 @@ namespace AirportBroadcast.Equipment.Service
             {
                 serviceController.Stop();
             }
+            MessageBox.Show("服务已停止。", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
+        /// <summary>
+        /// 暂停/恢复服务---好像没有用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSP_Click(object sender, EventArgs e)
         {
             ServiceController serviceController = new ServiceController(serverName);
@@ -62,20 +88,26 @@ namespace AirportBroadcast.Equipment.Service
                 if (serviceController.Status == ServiceControllerStatus.Running)
                 {
                     serviceController.Pause();
+                    MessageBox.Show("服务已暂停。", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else if (serviceController.Status == ServiceControllerStatus.Paused)
                 {
                     serviceController.Continue();
+                    MessageBox.Show("服务已恢复。", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
-
+        /// <summary>
+        /// 检查服务状态
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCheck_Click(object sender, EventArgs e)
         {
             ServiceController serviceController = new ServiceController(serverName);
             string Status = serviceController.Status.ToString();
             MessageBox.Show(String.Format("服务状态为：{0}", Status), "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-         
+
     }
 }
