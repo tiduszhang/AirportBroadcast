d5f70f50f2f649bd557b9d0687a6c411e0220481
zhangsx <zhangsx@nj-strongit.com.cn>
2019-12-18 08:32:42 +0800

    MQhelp

--- 代码/AirportBroadcast/AirportBroadcast.Equipment/Help/ActiveMQHelp.cs
+++ 代码/AirportBroadcast/AirportBroadcast.Equipment/Help/ActiveMQHelp.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace AirportBroadcast.Equipment
 {
-    class ActiveMQHelp
+    /// <summary>
+    /// ActiveMQ帮助类
+    /// </summary>
+    public class ActiveMQHelp
     {
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="message"></param>
+        public static void SendMessage(string message)
+        {
+            var configFile = System.IO.Path.Combine(System.Environment.CurrentDirectory, "tac.ini");
+            if (!System.IO.File.Exists(configFile))
+            {
+                return;
+            }
+            var activeMQUrl = IniFileHelp.Read("MQSERVER", "ActiveMQUrl", "", configFile);
+            var aueueName = IniFileHelp.Read("MQSERVER", "QueueName", "", configFile);
+            var password = IniFileHelp.Read("MQSERVER", "Password", "", configFile);
+            var userName = IniFileHelp.Read("MQSERVER", "UserName", "", configFile);
+
+            try
+            {
+                IConnectionFactory factory = new ConnectionFactory(String.Format("tcp://{0}/", activeMQUrl));
+                using (IConnection connection = factory.CreateConnection(userName, password))
+                {
+                    //Create the Session   
+                    using (ISession session = connection.CreateSession())
+                    {
+                        //Create the Producer for the topic/queue   
+                        IMessageProducer prod = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQTopic(aueueName));
+                        ITextMessage msg = prod.CreateTextMessage();
+                        msg.Text = message;
+                        prod.Send(msg);
+                        System.Threading.Thread.Sleep(1);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
     }
 }
